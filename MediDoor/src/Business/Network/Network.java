cbeb0cda0c45db9c0a8edceb9f3f1a6569d213da
Chandrakiran Gr <guthavariramesh.c@northeastern.edu>
2024-12-06 15:47:10 -0500

    Business order java wireframes and declarations

--- MediDoor/src/Business/Network/Network.java
+++ MediDoor/src/Business/Network/Network.java
@@ -4,10 +4,75 @@
  */
 package Business.Network;
 
+import Business.Customer.Customer;
+import Business.Customer.CustomerDirectory;
+import Business.Enterprise.EnterpriseDirectory;
+import java.util.ArrayList;
+
 /**
  *
  * @author chandrkiran
  */
 public class Network {
+    private String networkName;
+    private ArrayList<Integer> zipcodes;
+//    Customers 
+    private CustomerDirectory customerDirectory;
+    private EnterpriseDirectory enterpriseDirectory;
+    
+    public Network() {
+        this.zipcodes = new ArrayList<>();
+        this.enterpriseDirectory = new EnterpriseDirectory();
+        this.customerDirectory = new CustomerDirectory();
+    }
+
+    public CustomerDirectory getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(CustomerDirectory customerDirectory) {
+        this.customerDirectory = customerDirectory;
+    }
+    
+    public String getNetworkName() {
+        return networkName;
+    }
+
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public void setEnterpriseDirectory(EnterpriseDirectory enterpriseDirectory) {
+        this.enterpriseDirectory = enterpriseDirectory;
+    }
+    
+    private Customer checkCustomerInNetwork(String name) {
+        for(Customer cust: customerDirectory.getCustomerList()) {
+            if(cust.getUsername().equals(name)) {
+                return cust;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Integer> getZipcodes() {
+        return zipcodes;
+    }
+
+    public void setZipcodes(ArrayList<Integer> zipcodes) {
+        this.zipcodes = zipcodes;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return networkName;        
+    }
     
 }
+
