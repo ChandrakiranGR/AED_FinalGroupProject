aff7a1edfa61e89a6b343c34393193795fa7ba5f
sugurushetty.s <sugurushetty.s@northeastern.edu>
2024-12-07 18:17:50 -0500

    enterprise roles

--- MediDoor/src/Business/Enterprise/Role/EnterpriseAdminRole.java
+++ MediDoor/src/Business/Enterprise/Role/EnterpriseAdminRole.java
@@ -1,13 +1,25 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Enterprise.Role;
 
+import Business.Ecosystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
- * @author shivanisugurushetty
+ * @authorshivanisugurushetty
  */
-public class EnterpriseAdminRole {
-    
+public class EnterpriseAdminRole extends EnterpriseRole {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Network network, Organization organization, Enterprise enterprise, Ecosystem business) {
+//        return new DoctorWorkAreaJPanel(userProcessContainer, account, (DoctorOrganization)organization, enterprise); 
+        return null;
+    }
 }
