918efd160fa6128f1e87ad37ef53d806a9477d4f
sugurushetty.s <sugurushetty.s@northeastern.edu>
2024-12-07 18:22:03 -0500

    lab enterprise

--- MediDoor/src/Business/Enterprise/Role/LabEnterprise.java
+++ MediDoor/src/Business/Enterprise/Role/LabEnterprise.java
@@ -1,13 +1,27 @@
+
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Enterprise.Role;
+
+import Business.Ecosystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UI.LabEnterpriseRole.LabEnterpriseWorkAreaJPanel;
+//import UI.LabEnterpriseRole.LabEnterpriseWorkAreaJPanel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author shivanisugurushetty
  */
-public class LabEnterprise {
-    
+public class LabEnterprise extends EnterpriseRole {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Network network, Organization organization, Enterprise enterprise, Ecosystem business) {
+    return new LabEnterpriseWorkAreaJPanel(userProcessContainer,account,network,organization,enterprise,business);
+    }
 }
