09324c2f772e38ce9aa5d9e29b0da33b94467c3f
sugurushetty.s <sugurushetty.s@northeastern.edu>
2024-12-07 18:12:27 -0500

    Immunization and Lab Enterprise

--- MediDoor/src/Business/Enterprise/ImmunizationEnterprise.java
+++ MediDoor/src/Business/Enterprise/ImmunizationEnterprise.java
@@ -1,13 +1,28 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Enterprise;
+
+import Business.Enterprise.Role.EnterpriseRole;
+import Business.Enterprise.Role.LabEnterprise;
+import Business.Enterprise.Role.VaccinationEnterpriseRole;
+import java.util.ArrayList;
 
 /**
  *
  * @author shivanisugurushetty
  */
-public class ImmunizationEnterprise {
-    
+public class ImmunizationEnterprise extends Enterprise {
+     public ImmunizationEnterprise(String name, String location){
+        super(Enterprise.EnterpriseType.Vaccination, name, location);
+    }
+
+    public ArrayList<EnterpriseRole> getSupportedRole() {
+        ArrayList<EnterpriseRole> roles = new ArrayList();
+        roles.add(new VaccinationEnterpriseRole());
+        
+        return roles;
+    }
 }
--- MediDoor/src/Business/Enterprise/LabOrgDiagnosticsEnterprise.java
+++ MediDoor/src/Business/Enterprise/LabOrgDiagnosticsEnterprise.java
@@ -1,13 +1,29 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Enterprise;
+
+import Business.Enterprise.Enterprise.EnterpriseType;
+import Business.Enterprise.Role.EnterpriseRole;
+import Business.Enterprise.Role.LabEnterprise;
+import Business.Enterprise.Role.PharmaceuticalEnterpriseAdminRole;
+import java.util.ArrayList;
 
 /**
  *
  * @author shivanisugurushetty
  */
-public class LabOrgDiagnosticsEnterprise {
-    
+public class LabOrgDiagnosticsEnterprise extends Enterprise {
+    public LabOrgDiagnosticsEnterprise(String name, String location){
+        super(EnterpriseType.LabDiagnostics, name, location);
+    }
+
+    public ArrayList<EnterpriseRole> getSupportedRole() {
+        ArrayList<EnterpriseRole> roles = new ArrayList();
+        roles.add(new LabEnterprise());
+        
+        return roles;
+    }
 }
